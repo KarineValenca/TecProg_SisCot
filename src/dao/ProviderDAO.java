35505bffc3d9a1cba7e36aef636ab4723416a53f
Karine Valença <valenca.karine@gmail.com>
2016-04-05 22:22:50 -0300

    add asserts to ProviderDAO class

--- src/dao/ProviderDAO.java
+++ src/dao/ProviderDAO.java
@@ -16,6 +16,7 @@ public class ProviderDAO {
 	}
 	
 	public boolean insertProvider(Provider provider) {
+		assert(provider != null) : "the provider object is null";
 		String sql = "insert into Providers (cnpj, nome, email, senha, ddd, "
 				+ "telefone, endereco, cidade, estado, cep, authorized) "
 				+ "values (?,?,?,?,?,?,?,?,?,?,?)";
@@ -84,6 +85,7 @@ public class ProviderDAO {
 	}
 	
 	public boolean deleteProvider(String providerCnpj) {
+		assert(providerCnpj != null) : "the provider cnpj is null";
 		String sql = "delete from Providers where cnpj = ?";
 		boolean wasDeleted = false;
 
@@ -103,7 +105,9 @@ public class ProviderDAO {
 		return wasDeleted;
 	}
 	
-	public boolean updateProvider(String cnpjToUpdate, Provider provider) {		
+	public boolean updateProvider(String cnpjToUpdate, Provider provider) {
+		assert(cnpjToUpdate != null) : "the cnpj is null";
+		assert(provider != null) : "the provider is null";
 		String sql = "update Providers set cnpj=?, nome=?, email=?, senha=?, ddd=?, "
 				+ "telefone=?, endereco=?, cidade=?, estado=?, cep=?, authorized=? where cnpj=?";
 		boolean wasUpdated = false;
