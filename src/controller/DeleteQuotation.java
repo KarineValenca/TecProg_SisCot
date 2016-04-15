478278bf669cc00c000a8d2e0930df6fd7100386
Murilo Duarte <muriloduartegoncalves@hotmail.com>
2016-04-15 17:40:15 -0300

    Add asserts in DeleteQuotation

--- src/controller/DeleteQuotation.java
+++ src/controller/DeleteQuotation.java
@@ -14,40 +14,41 @@ import dao.QuotationDAO;
 @WebServlet("/DeleteQuotation")
 public class DeleteQuotation extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+
 	public DeleteQuotation() {
 		super();
 	}
-	
+
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws
 		ServletException, IOException {
 		int quotationId = Integer.parseInt(request.getParameter("quotationId"));
 		String messageDeletedConfirmation = "Iniciada";
-		
+
 		boolean wasDeleted = deleteQuotation(quotationId);
-		
+
 		if(wasDeleted) {
 			messageDeletedConfirmation = "Cotação excluída com sucesso!";
 		}
 		else {
 			messageDeletedConfirmation = "Erro! A cotação não pôde ser excluída.";
 		}
-		
+
 		request.setAttribute("mensage", messageDeletedConfirmation);
-		
-		//Dispacher the result from the view of confirmation		
+
+		//Dispacher the result from the view of confirmation
 		RequestDispatcher rd;
 		rd = request.getRequestDispatcher("/QuotationResponse.jsp");
 	    rd.forward(request,response);
 	}
-	
+
 	public boolean deleteQuotation(int quotationId) {
+		assert(quotation >= 0) : "unexpected error: the quotation is less than 0";
 		boolean wasDeleted = false;
-		
+
 		QuotationDAO quotationDao = new QuotationDAO();
 		System.out.println("ID da cotação");
 		wasDeleted = quotationDao.deleteQuotation(quotationId);
-		
+
 		return wasDeleted;
 	}
 
