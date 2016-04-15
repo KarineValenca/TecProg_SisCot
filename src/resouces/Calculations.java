ca73328e3664741ee0066d2ea5d97c762824790f
Murilo Duarte <muriloduartegoncalves@hotmail.com>
2016-04-15 17:16:30 -0300

    Include asserts in Calculation and ProductDAO. Change attribute nome in deleteProduct method to name in ProductDAO

--- src/resouces/Calculations.java
+++ src/resouces/Calculations.java
@@ -3,6 +3,7 @@ package resouces;
 public class Calculations {
 
 	public static String formatDecimal(Double number) {
+                         assert(number >=0) : "unexpected error: the informed number is invalid";
 		Double epsilon = new Double(0.004f); // 4 tenths of a cent
 
 		return String.format("%10.2f", number); // dj_segfault
