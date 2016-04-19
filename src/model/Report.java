093283b1129efd684b468616e1a4c60a4d8a29be
Murilo Duarte <muriloduartegoncalves@hotmail.com>
2016-04-19 13:44:16 -0300

    applied style sheet to Report MODEL

--- src/model/Report.java
+++ src/model/Report.java
@@ -1,24 +1,29 @@
+/*
+ * Class name: Publication.java
+ * Purpose of class: This class is used to store all attributes from report.
+ * Copyright: This software follows GPL license.
+ */
+
 package model;
 
 import java.util.ArrayList;
 
 public class Report {
-
 	private ArrayList<Product> products;
 	private Quotation quotation;
 	private double totalPrice;
-	
+
 	public Report() {
-		
+
 	}
-	
+
 	public Report(ArrayList<Product> products, Quotation quotation, double totalPrice) {
 		super();
 		this.products = products;
 		this.quotation = quotation;
 		this.totalPrice = totalPrice;
 	}
-	
+
 	public ArrayList<Product> getProducts() {
 		return products;
 	}
@@ -37,9 +42,8 @@ public class Report {
 	public void setTotalPrice(double totalPrice) {
 		this.totalPrice = totalPrice;
 	}
-	
 	public ArrayList<ArrayList> showProducts(){
-		
+
 		return null;
 	}
 }
