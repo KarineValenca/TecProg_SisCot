99350aca49002ea2fa91d8b5457aa9a9fc1abd65
Murilo Duarte <muriloduartegoncalves@hotmail.com>
2016-04-20 13:25:27 -0300

    applied style sheet to Provider MODEL and uncomplicating code

--- src/model/Provider.java
+++ src/model/Provider.java
@@ -1,3 +1,9 @@
+/*
+ * Class name: Provider.java
+ * Purpose of class: This class is used to store all attributes from provider.
+ * Copyright: This software follows GPL license.
+ */
+
 package model;
 
 public class Provider {
@@ -18,92 +24,87 @@ public class Provider {
 		super();
 	}
 
-	public String getProviderCnpj() {
-		return providerCnpj;
-	}
-
-	public void setProviderCnpj(String providerCnpj) {
+	public Provider(String providerCnpj, String providerName, String providerEmail,
+		            String providerPassword, int providerDdd, int providerPhone,
+		            String ProviderAdress, String ProviderCity, String ProviderState,
+		            int ProviderZip, boolean authorized) {
 		this.providerCnpj = providerCnpj;
-	}
-
-	public String getProviderName() {
-		return providerName;
-	}
-
-	public void setProviderName(String providerName) {
 		this.providerName = providerName;
-	}
-
-	public String getProviderEmail() {
-		return providerEmail;
-	}
-
-	public void setProviderEmail(String providerEmail) {
 		this.providerEmail = providerEmail;
-	}
-
-	public String getProviderPassword() {
-		return providerPassword;
-	}
-
-	public void setProviderPassword(String providerPassword) {
 		this.providerPassword = providerPassword;
-	}
-
-	public int getProviderDdd() {
-		return providerDdd;
-	}
-
-	public void setProviderDdd(int providerDdd) {
 		this.providerDdd = providerDdd;
-	}
-
-	public int getProviderPhone() {
-		return providerPhone;
-	}
-
-	public void setProviderPhone(int providerPhone) {
 		this.providerPhone = providerPhone;
-	}
-
-	public String getProviderAdress() {
-		return ProviderAdress;
-	}
-
-	public void setProviderAdress(String ProviderAdress) {
 		this.ProviderAdress = ProviderAdress;
-	}
-
-	public String getProviderCity() {
-		return ProviderCity;
-	}
-
-	public void setProviderCity(String ProviderCity) {
-		this.ProviderCity = ProviderCity;
-	}
-
-	public String getProviderState() {
-		return ProviderState;
-	}
-
-	public void setProviderState(String ProviderState) {
+		this. ProviderCity = ProviderCity;
 		this.ProviderState = ProviderState;
-	}
-
-	public int getProviderZip() {
-		return ProviderZip;
-	}
-
-	public void setProviderZip(int ProviderZip) {
 		this.ProviderZip = ProviderZip;
-	}
-	
-	public boolean isAuthorized() {
-		return authorized;
-	}
-
-	public void setAuthorized(boolean authorized) {
 		this.authorized = authorized;
 	}
 
+	public String getProviderCnpj() {
+		return providerCnpj;
+	}
+	public void setProviderCnpj(String providerCnpj) {
+		this.providerCnpj = providerCnpj;
+	}
+	public String getProviderName() {
+		return providerName;
+	}
+	public void setProviderName(String providerName) {
+		this.providerName = providerName;
+	}
+	public String getProviderEmail() {
+		return providerEmail;
+	}
+	public void setProviderEmail(String providerEmail) {
+		this.providerEmail = providerEmail;
+	}
+	public String getProviderPassword() {
+		return providerPassword;
+	}
+	public void setProviderPassword(String providerPassword) {
+		this.providerPassword = providerPassword;
+	}
+	public int getProviderDdd() {
+		return providerDdd;
+	}
+	public void setProviderDdd(int providerDdd) {
+		this.providerDdd = providerDdd;
+	}
+	public int getProviderPhone() {
+		return providerPhone;
+	}
+	public void setProviderPhone(int providerPhone) {
+		this.providerPhone = providerPhone;
+	}
+	public String getProviderAdress() {
+		return ProviderAdress;
+	}
+	public void setProviderAdress(String ProviderAdress) {
+		this.ProviderAdress = ProviderAdress;
+	}
+	public String getProviderCity() {
+		return ProviderCity;
+	}
+	public void setProviderCity(String ProviderCity) {
+		this.ProviderCity = ProviderCity;
+	}
+	public String getProviderState() {
+		return ProviderState;
+	}
+	public void setProviderState(String ProviderState) {
+		this.ProviderState = ProviderState;
+	}
+	public int getProviderZip() {
+		return ProviderZip;
+	}
+	public void setProviderZip(int ProviderZip) {
+		this.ProviderZip = ProviderZip;
+	}
+	public boolean isAuthorized() {
+		return authorized;
+	}
+	public void setAuthorized(boolean authorized) {
+		this.authorized = authorized;
+	}
 }
