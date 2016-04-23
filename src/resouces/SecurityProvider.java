ec3a4efdb309615c400499e2d95d95d45580dfd8
Attany <nathalyaraujoo@gmail.com>
2016-04-23 17:45:58 -0300

    Add comments to SecurityProvider class

--- src/resouces/SecurityProvider.java
+++ src/resouces/SecurityProvider.java
@@ -1,3 +1,8 @@
+/*
+ * Class name: SecurityProvider.java
+ * Purpose of class: This class is a implementation of Filter interface class.
+ * Copyright: This software follows GPL license.
+ */
 package resouces;
 
 import java.io.IOException;
@@ -13,35 +18,20 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-/**
- * Servlet Filter implementation class SecurityProvider
- * 
- * @author tiago
- * @version 1.0
- */
-@WebFilter(urlPatterns = {"*.jsp", "*.html", "/ConsultProduct",
-		"/ConsultProvider", "/DeleteProduct", "/DeleteProvider",
-		"/IncludeProduct", "/IncludeProvider", "/UpdateProduct",
-		"/UpdateProvider"})
 public class SecurityProvider implements Filter {
 
-	/**
-	 * Default constructor.
-	 */
+	// this method is a constructor method of the class.
 	public SecurityProvider() {
 
 	}
 
-	/**
-	 * @see Filter#destroy()
-	 */
+	// this method is a filter destroy. (Epmty method)
 	public void destroy() {
 
 	}
 
-	/**
-	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
-	 */
+	/*this method is responsible to Verify if the current user have a session 
+	setted.*/
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain chain) throws IOException, ServletException {
 		assert (request != null) : "The request from client is null";
@@ -70,15 +60,14 @@ public class SecurityProvider implements Filter {
 		}
 	}
 
+	// this method set a session.
 	private HttpSession setSession(HttpSession session) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
-	/**
-	 * @see Filter#init(FilterConfig)
-	 */
+	// this method is a filter init. (Epmty method)
 	public void init(FilterConfig fConfig) throws ServletException {
+		
 	}
 
 }
