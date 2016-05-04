d7f20c35b1c04f3e1b11234fec7b4b59ab0dd513
Keli Cristina <keliunb@gmail.com>
2016-05-03 23:08:58 -0300

    include comments of file ManagerDAO.java and fix comments of ManagerDAO class and  methods

--- src/dao/ManagerDAO.java
+++ src/dao/ManagerDAO.java
@@ -1,9 +1,8 @@
-/*
- * Class name: ManagerDAO.java
- * Purpose of class: This class is used to insert, delete, update or list a 
- * manager.
- * Copyright: This software follows GPL license.
- */
+/** 
+* File name: ManagerDAO.java
+* Purpose of file: This file contains the ManagerDAO class and its methods.   
+* Copyright: This software follows GPL license.
+**/
 
 package dao;
 
@@ -12,18 +11,34 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-
 import model.Manager;
 
+/**
+* Class name: ManagerDAO
+* Purpose of class: This class is used to do all actions in the database 
+* relate to manager: insert, delete, update or list a manager.
+**/
 public class ManagerDAO {
 	private Connection connection;
-	
+
+    /** 
+    * Method name: ManagerDAO
+    * Purpose of method: This method is used to connection in database.  
+    * @param: there is no param.
+    * @return: there is no return.
+    **/
 	public ManagerDAO() {
 		this.connection = ConnectionDB.getConnection().connectionWithDataBase;
 	}
 
-	/* this method is responsible for entering username and password of a 
-	manager in the database.*/
+    /** 
+    * Method name: insertManager
+    * Purpose of method: This method is responsible for entering username and
+    * password of a	manager in the database, it returns a boolean that returns 
+    * true if the manager was added.  
+    * @param manager: is instance of Manager to be inserted in the database.
+    * @return: there is no return.
+    **/
 	public boolean insertManager(Manager manager) {
 		assert (manager != null) : "unexpected error: the manager object is "
 									+ "null";
@@ -41,12 +56,16 @@ public class ManagerDAO {
 			statement.close();
 		} catch(SQLException e) {
 			throw new RuntimeException(e);
-		}
-		
+		}	
 		return wasAdded;
 	}
-
-	// this method is responsible for bringing a list managers of database. 	
+ 
+	/** 
+    * Method name: listManagers
+    * Purpose of method: This method is responsible for bringing a list managers of database.  
+    * @param : there is no param.
+    * @return managers: return list of Manager.
+    **/	
 	public ArrayList<Manager> listManagers() {
 		String sql;
 		sql = "select * from Managers";
@@ -71,8 +90,17 @@ public class ManagerDAO {
 		
 		return managers;
 	}
-	
-	// this method is responsible for delete a manager of database.
+
+	/** 
+	* Method name: deleteManager
+    * Purpose of method: This method is used to delete a manager at the 
+    * database, it returns a boolean that returns true if the manager was 
+    * deleted.  
+    * @param managerUsername: this string gets the user name of manager from 
+    * the view to be deleted in the database.
+    * @return wasDeleted: this boolean value is used to verify if the manager
+     * was deleted.
+    **/		
 	public boolean deleteManager(String managerUsername) {
 		assert(managerUsername != null) : "unexpected error: the manager user "
 										   + "name is null";
@@ -94,8 +122,18 @@ public class ManagerDAO {
 		
 		return wasDeleted;
 	}
-	
-	// this method is responsible for update a manager of database.	
+
+	/** 
+	* Method name: updateManager
+    * Purpose of method: This method is used to update a manager at the 
+    * database, it returns a boolean that returns true if the manager was 
+    * updated.  
+    * @param usernameToUpdate: This string gets the user name of manager to 
+    * update from the view.
+    * @param manager: is instance of Manager to be updated in the database.
+    * @return wasDeleted: this boolean value is used to verify if the manager
+     * was deleted.
+    **/	
 	public boolean updateManager(String usernameToUpdate, Manager manager) {
 		assert(usernameToUpdate != null) : "unexpected error: the user name to "
 											+ "update is null";
