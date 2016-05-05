adce49d006d08e2e9b5034108dbcf0547df51565
Karine Valença <valenca.karine@gmail.com>
2016-05-04 21:35:35 -0300

    Update comments at DeleteProvider

--- src/controller/DeleteProvider.java
+++ src/controller/DeleteProvider.java
@@ -1,8 +1,8 @@
-/*
- * Class name: DeleteProvider.java
- * Purpose of class: This class is used to exclude a provider from database.
+/**
+ * File name: DeleteProvider.java 
+ * Purpose of file: This file contains the DeleteProvider class and its methods.
  * Copyright: This software follows GPL license.
- */
+ **/
 
 package controller;
 
@@ -17,6 +17,10 @@ import javax.servlet.http.HttpServletResponse;
 
 import dao.ProviderDAO;
 
+/**
+ * Class name: DeleteProvider
+ * Purpose of class: This class is used to exclude a provider from database.
+ **/
 @WebServlet("/DeleteProvider")
 public class DeleteProvider extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -25,6 +29,16 @@ public class DeleteProvider extends HttpServlet {
 		super();
 	}
 	
+	/**
+	 * Method name: service
+	 * Purpose of method: This method is used to get some values from view and 
+	 * pass the result of the method deleteProvider to view.
+	 * @param request: this attribute is used to represent the HTTP request 
+	 * that a browser sends to the application. 
+	 * @param response: this attribute is used to represent the HTTP response
+	 * that the application sends to a browser.
+	 * @return: there is no return.
+	**/
 	protected void service(HttpServletRequest request, 
 							HttpServletResponse response) 
 							throws ServletException, IOException {
@@ -45,13 +59,21 @@ public class DeleteProvider extends HttpServlet {
 		
 		request.setAttribute("mensage", messageDeletedConfirmation);
 		
-		//Dispacher the result from the view of confirmation		
+		//Dispatcher the result from the view of confirmation		
 		RequestDispatcher rd;
 		
 		rd = request.getRequestDispatcher("/ProviderResponse.jsp");
 	    rd.forward(request,response);
 	}
 	
+	/**
+	 * Method name: deleteProvider
+	 * Purpose of method: This method is used to delete a provider from database.
+	 * @param providerName: this attribute is the name of the provider that will
+	 * be deleted.
+	 * @return: it returns true if the provider was deleted, and returns false
+	 * if the provider wasn't deleted.
+	**/
 	public boolean deleteProvider(String providerName) {
 		assert(providerName != null) : "The provider name is recieving null";
 		
