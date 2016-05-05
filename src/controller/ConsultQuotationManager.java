f90465ef504ad286661a609c40c457ce1d3fc3c5
Karine Valença <valenca.karine@gmail.com>
2016-05-04 21:19:01 -0300

    Update comments at ConsultQuotationManager

--- src/controller/ConsultQuotationManager.java
+++ src/controller/ConsultQuotationManager.java
@@ -1,10 +1,9 @@
-/*
- * Class name: ConsultQuotationManager.java
- * Purpose of class: This class is responsible by consult the list of quotations,
- * it means, that by this class, a manager can see the price of a product at the
- * market.
+/**
+ * File name: ConsultQuotationManager.java 
+ * Purpose of file: This file contains the ConsultQuotationManager class and 
+ * its methods.
  * Copyright: This software follows GPL license.
- */
+ **/
 
 package controller;
 
@@ -20,13 +19,26 @@ import javax.servlet.http.HttpServletResponse;
 import model.Quotation;
 import dao.QuotationDAO;
 
+/*
+ * Class name: ConsultQuotationManager
+ * Purpose of class: This class is responsible by consult the list of quotations,
+ * it means, that by this class, a manager can see the price of a product at the
+ * market.
+ */
 @WebServlet("/ConsultQuotationManager")
 public class ConsultQuotationManager extends ConsultQuotation {
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Method name: service
+	 * Purpose of method: This method is used to get the values of the view
+	 * and call the method buildQuotationList.
+	 * @param request: this attribute is used to represent the HTTP request 
+	 * that a browser sends to the application. 
+	 * @param response: this attribute is used to represent the HTTP response
+	 * that the application sends to a browser.
+	 * @return: there is no return.
+	**/
 	@Override
 	protected void service(HttpServletRequest request, 
 							HttpServletResponse response) 
@@ -45,6 +57,14 @@ public class ConsultQuotationManager extends ConsultQuotation {
         rd.forward(request,response);
 	}
 
+	/**
+	 * Method name: service
+	 * Purpose of method: This method is used to consult the list of quotations
+	 * in the database.
+	 * @param: there is no param.
+	 * @return: it return an arraylist of quotation type with all quotations 
+	 * related to a manager.
+	**/
 	@Override
 	protected ArrayList<Quotation> buildQuotationList() {
 		ArrayList<Quotation> quotationList = new ArrayList<Quotation>();
