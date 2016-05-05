21347624a388c7b91c0a170ed32ccfc736bcf8b0
Karine Valença <valenca.karine@gmail.com>
2016-05-04 21:12:05 -0300

    Update comments at ConsultProvider

--- src/controller/ConsultProvider.java
+++ src/controller/ConsultProvider.java
@@ -1,8 +1,8 @@
-/*
- * Class name: ConsultProvider.java
- * Purpose of class: This class is used to do a consult at providers data, and list all providers.
+/**
+ * File name: ConsultProvider.java 
+ * Purpose of file: This file contains the ConsultProvider class and its methods.
  * Copyright: This software follows GPL license.
- */
+ **/
 
 package controller;
 
@@ -20,20 +20,28 @@ import dao.ProviderDAO;
 import model.Provider;
 
 /**
- * Servlet implementation class ConsultProvider
- */
-
+* Class name: ConsultProvider
+* Purpose of class: This class is used to do a consult at providers data, and 
+* list all providers.
+**/
 @WebServlet("/ConsultProvider")
 public class ConsultProvider extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+ 
     public ConsultProvider() {
         super();
     }
     
+    /**
+	 * Method name: service
+	 * Purpose of method: By this method a consult at database is done and the 
+	 * providers list is obtained.
+	 * @param request: this attribute is used to represent the HTTP request 
+	 * that a browser sends to the application. 
+	 * @param response: this attribute is used to represent the HTTP response
+	 * that the application sends to a browser.
+	 * @return: there is no return.
+	 **/
     protected void service(HttpServletRequest request, 
     						HttpServletResponse response) 
     						throws ServletException, IOException {
