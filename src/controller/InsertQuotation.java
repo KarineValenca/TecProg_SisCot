517f9db5c8a57949e7cfdedb4f3d743047bba4e8
Murilo Duarte <muriloduartegoncalves@hotmail.com>
2016-05-06 00:48:50 -0300

    insert comment InsertQuotation SERVLET file

--- src/controller/InsertQuotation.java
+++ src/controller/InsertQuotation.java
@@ -1,8 +1,8 @@
-/*
- * Class name: InsertQuotation.java
+/**
+ * File name: InsertQuotation.java
  * Purpose of class: This class is used to enter new quotes, and their products.
  * Copyright: This software follows GPL license.
- */
+ **/
 
 package controller;
 
@@ -23,27 +23,34 @@ import model.Quotation;
 import dao.ProductDAO;
 import dao.QuotationDAO;
 
-
 /**
- * Servlet implementation class IncludeProduct
- */
+ * Class name: InsertQuotation
+ * Purpose of class: Servlet implementation class IncludeProduct
+ **/
 @WebServlet("/IncludeQuotation")
 public class InsertQuotation extends HttpServlet{
 	private static final long serialVersionUID = 1L;
 
-
-   /**Creating new Quotation
-    * @see HttpServlet#HttpServlet()
-    */
+	/**
+            * Method name: insertQuotation
+            * Purpose of method: Creating new Quotation
+            * @see HttpServlet#HttpServlet()
+            **/
 	public InsertQuotation() {
 		super();
 	}
 
 	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
-	 */
+            * Method name: service
+            * Purpose of method: This method is used to get some values from view and
+            * pass the result of the method insertProvider to view.
+            * @param request: used to represent the HTTP request that a browser sends
+            * to the application.
+            * @param response: used to represent the HTTP response that the application
+            * sends to a browser.
+            **/
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws
-		                        ServletException, IOException {
+	ServletException, IOException {
 
 		String messageAddConfirmation = "Iniciada";
 		HttpSession session = request.getSession();
@@ -73,9 +80,17 @@ public class InsertQuotation extends HttpServlet{
 		//Dispacher the result from the view of confirmation
 		RequestDispatcher rd;
 		rd = request.getRequestDispatcher("/QuotationResponse.jsp");
-       		rd.forward(request,response);
+		rd.forward(request,response);
 	}
 
+          /**
+     	* Method name: insertQuotation
+    	* Purpose of method: This method is add the new quotation into the database
+    	* and verify if the quotation was add successfully.
+    	* @param quotation: this object quotation to be inserted in the database.
+     	* @return wasAdd: this boolean value is used to verify if the quotation
+    	* was inserted.
+     	**/
 	public int insertQuotation(Quotation quotation) {
 		assert(quotation != null) : "unexpected error: the quotation is null";
 		int id;
@@ -86,6 +101,12 @@ public class InsertQuotation extends HttpServlet{
 		return id;
 	}
 
+	/**
+     	* Method name: selectProducts
+    	* Purpose of method: Select the products of quotation
+	* @param request: this attribute is used to represent the HTTP request
+    	* @param quotation: this object quotation to be inserted in the database.
+     	**/
 	private void selectProducts(HttpServletRequest request, Quotation quotation) {
 		assert(request != null) : "unexpected error: the request is null";
 		assert(quotation != null) : "unexpected error: the quotation is null";
@@ -111,6 +132,13 @@ public class InsertQuotation extends HttpServlet{
 		}
 	}
 
+	/**
+     	* Method name: createNewCotation
+    	* Purpose of method: create a new quote products
+	* @param managerName: this attribute is name of the creator of the quotation
+    	* @param quotationDate: date of quotation
+    	* @return quotation: the object quotation created
+     	**/
 	private Quotation createNewCotation(String managerName, Date quotationDate){
 		assert(managerName != null) : "unexpected error: the managerName is null";
 		assert(quotationDate != null) : "unexpected error: the quotationDate is null";
