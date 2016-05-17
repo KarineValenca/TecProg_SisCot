7ff3845e814b966ef48c3eee328341820a67b4e1
Murilo Duarte <muriloduartegoncalves@hotmail.com>
2016-05-17 14:41:47 -0300

    Revert "Correct position variables in Calculation SERVLET"
    This reverts commit 3f5f9aac89a92e50b42287727f42f7c77ab790d8.

--- src/resouces/Calculations.java
+++ src/resouces/Calculations.java
@@ -2,26 +2,26 @@
  * File name: Calculations.java
  * Purpose of class: Allows the calculation of quotes values
  * Copyright: This software follows GPL license.
- */
+ **/
 
 package resouces;
 
 /**
  * Class name: Calculations.java
  * Purpose of class: Class calculation in format values decimal
- */
+ **/
 public class Calculations {
-    /**
-     * Method name: formatDecimal
-     * Purpose of method: This method formats a quotation for decimal
-     * @param number: This attribute is input from a quote value
-     * @return: returns the price in decimal
-     */
-	public static String formatDecimal(Double number) {
-		assert(number >= 0) : "unexpected error: the informed number is invalid";
-		
-		String formatNumber = String.format("%10.2f", number);
+            /**
+             * Method name: formatDecimal
+             * Purpose of method: This method formats a quotation for decimal
+             * @param number: This attribute is input from a quote value
+             * @return: returns the price in decimal
+            **/
+        	public static String formatDecimal(Double number) {
+                         assert(number >=0) : "unexpected error: the informed number is invalid";
+            		      Double epsilon = new Double(0.004f); // 4 tenths of a cent
+                         String formatNumber = String.format("%10.2f", number);
 
-		return formatNumber; // dj_segfault
+            		      return formatNumber; // dj_segfault
       	}
 }
