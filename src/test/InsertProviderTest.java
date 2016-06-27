2fc967c68d7001a912c0a5d0299acf341356fd23
Keli Cristina Borges <keliunb@gmail.com>
2016-06-26 19:32:16 -0700

    fix test in InsertProviderTest

--- src/test/InsertProviderTest.java
+++ src/test/InsertProviderTest.java
@@ -12,6 +12,42 @@ public class InsertProviderTest {
 	InsertProvider insertProvider = new InsertProvider();
 	
 	@Test
+	public void testNameProvidertNull() {
+		String nameProviderNull = null;
+		
+		provider.setProviderCnpj("123456789");
+		provider.setProviderName(nameProviderNull);
+		provider.setProviderEmail("carrefour@gmail.com");
+		provider.setProviderPassword("senha123");
+		provider.setProviderDdd(61);
+		provider.setProviderPhone(12345678);
+		provider.setProviderAdress("Quadra 10 Bloco 1");
+		provider.setProviderCity("Brasilia");
+		provider.setProviderState("DF");
+		provider.setProviderZip(70000123);
+		provider.setAuthorized(true);
+		
+		assertFalse(insertProvider.insertProvider(provider));
+	}
+
+	@Test
+	public void testAuthorizedFalse() {		
+		provider.setProviderCnpj("123456789");
+		provider.setProviderName("Carrefour");
+		provider.setProviderEmail("carrefour@gmail.com");
+		provider.setProviderPassword("senha123");
+		provider.setProviderDdd(61);
+		provider.setProviderPhone(12345678);
+		provider.setProviderAdress("Quadra 10 Bloco 1");
+		provider.setProviderCity("Brasilia");
+		provider.setProviderState("DF");
+		provider.setProviderZip(70000123);
+		provider.setAuthorized(false);
+		
+		assertTrue(insertProvider.insertProvider(provider));
+	}
+	
+	@Test
 	public void testInsertProvider() {
 		provider.setProviderCnpj("123456789");
 		provider.setProviderName("Carrefour");
